862c7e3d68916a83f79e2684b84aad622385660a
Sufyan Lodhi <sufyanlodhi@hotmail.com>
2025-06-15 16:51:20 +0500

    Updated project

--- 01inputandoutput/src/App.jsx
+++ 01inputandoutput/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useCallback, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const [fruits, setFruits] = useState(["Banana", "Orange"]);
+  const [fruits, setFruits] = useState([]);
   const [recval, setRecval] = useState('');
   const inputref = useRef();
 
@@ -12,33 +12,51 @@ function App() {
     }
   }, [recval]);
 
-  const addFruits = useCallback(() => {
-    inputref.current.focus(); // 😎 yahan par main ne focua kia input ko 
 
+
+  const addFruits = useCallback(() => {
+    inputref.current.focus(); // 😎 yahan par main ne focus kia input ko 
     if (recval) {
       if (fruits.includes(recval)) {
         alert("Your value is same. Please enter another value");
       } else {
-        setFruits([...fruits, recval]); 
-        setRecval(''); 
+        setFruits([...fruits, recval]);
+        console.log([...fruits, recval]);
+        setRecval('');
       }
     } else {
       alert("Please Enter any Fruit name");
     }
-    //console.log("Inside function");
-    //console.log(fruits); 
-  },[recval, fruits]);
+    // console.log("Inside function");
+    // console.log(fruits); 
+  }, [recval, fruits]);
 
 
-  //console.log(addFruits);
-  
+
+  {/* 😎 yahan par main ne filter lagaya hai ta k jo map main loop chala hai index 
+  main jo bhi number hai uska match karo puri fruits list se jo 
+  match hojaye wo false kr k out kr or jo match na ho usey updated fruits new list main add kro */}
+
+  function removevalue(index) {
+    const updatedfruits = fruits.filter((ele, id) => {
+      return index != id;
+    })
+    setFruits(updatedfruits)
+    console.log(updatedfruits);
+  }
+
+
+
   return (
     <>
-      <ol>
+      <div>
         {fruits.map((value, index) =>
-          <li key={index}>{value}</li>
+          <p key={index}>{value}
+            <button onClick={() => removevalue(index)}>Remove</button></p>
         )}
-      </ol>
+      </div>
+
+
       <input
         ref={inputref}
         type="text"
@@ -46,6 +64,8 @@ function App() {
         onChange={(e) => setRecval(e.target.value)}
       />
       <br /><br />
+
+
       <button onClick={addFruits}>Add Fruits</button>
     </>
   );
