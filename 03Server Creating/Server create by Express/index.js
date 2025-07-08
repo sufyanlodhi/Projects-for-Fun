4cd87b17815d246df3feaa631ebfb885c9827217
Sufyan Lodhi <sufyanlodhi@hotmail.com>
2025-07-08 23:10:51 +0500

    Express server is updated

--- 03Server Creating/Server create by Express/index.js
+++ 03Server Creating/Server create by Express/index.js
@@ -1,18 +1,18 @@
-let express = require ("express")
+const express = require("express");
+const app = express();
 
-let app = express()
+app.get("/", (req, res) => {
+    res.send({ status: 1, msg: "Home page" });
+});
 
-app.get("/", (req,res)=>{
-    res.send({status: 1, msg: "Home page"})
-})
+app.get("/about", (req, res) => {
+    res.send("Welcome to about");
+});
 
+app.get("/contact", (req, res) => {
+    res.send("Welcome to Contact");
+});
 
-app.get("/about", (req,res)=>{
-    res.send("Welcome to about")
-})
-
-app.get("/contact", (req,res)=>{
-    res.send("Welcome to Contact")
-})
-
-app.listen("8000")
+app.listen(8000, () => {
+    console.log("Server is running on http://localhost:8000");
+});
